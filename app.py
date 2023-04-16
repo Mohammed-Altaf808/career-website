83e773985436d7130699ea945609666134dfccb9
Mohammed Altaf <114064230+Mohammed-Altaf808@users.noreply.github.com>
2023-04-16 11:29:05 +0000

    dynamic data

--- app.py
+++ app.py
@@ -2,9 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title': 'data analyst',
+    'location': 'india',
+    'salary': 'Rs 12,00,000',
+  },
+  {
+    'id':2,
+    'title': 'data engineer',
+    'location': 'india',
+    'salary': 'Rs 10,00,000',
+  },
+  { 
+    'id':3,
+    'title': 'web developer',
+    'location': 'india',
+    'salary': 'Rs 15,00,000',
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template('home.html',
+                           jobs=JOBS)
 
 if __name__ =="__main__":
   app.run(host='0.0.0.0', debug=True)
